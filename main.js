69edd6108e46e13f57d4649c515cb61595d45d22
Thippharake-NaChiengmai <thippharake_na@cmu.ac.th>
2025-06-27 01:32:28 +0700

    point 8.5

--- main.js
+++ main.js
@@ -6,55 +6,75 @@ const app = createApp({
         // const product = ref('Socks');
         const product = ref('Boots');
         const productDescription = ref('A pair of warm and comfortable boots for winter.');
-        const image = ref('./assets/images/socks_green.jpg');
+        const brand = ref('SE 331');
+        const image = computed(() => {
+            return variants.value[selectedVariant.value].image;
+        });
         const productLink = ref('https://www.camt.cmu.ac.th');
-         const inStock = computed(() => inventory.value > 0);
+        const inStock = computed(() => {
+            return variants.value[selectedVariant.value].quantity;
+        });
         const inventory = ref(100);
-        const onSale = computed(() => inventory.value > 0);
+        const onSale = computed(() => {
+            return variants.value[selectedVariant.value].quantity;
+        });
+        const saleMessage = computed(() =>
+  onSale.value ? `${brand.value} ${product.value} is on sale!` : ''
+)
         const details = ref([
             '50% cotton',
             '30% wool',
             '20% polyester']);
         const variants = ref([
             {
-                id: 2234, color: 'green', image: './assets/images/socks_green.jpg'
+                id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50
             },
             {
-                id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg'
+                id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0
             }
         ]);
-
-
+        const selectedVariant = ref(0);
         const sizes = ref(['S', 'M', 'L']);
         const sizesInLine = computed(() => sizes.value.join('/'));
-         const cart = ref(0);
+        const cart = ref(0);
+        const title = computed(() => {
+            return brand.value + ' ' + product.value;
+        });
         function addToCart() {
             cart.value += 1;
         }
-         function updateImage(variantImage) {
+        function updateImage(variantImage) {
             image.value = variantImage;
         }
 
-         function toggleStock () {
-      inventory.value = inventory.value > 0 ? 0 : 100;
-    }
+        function updateVariant(index) {
+            selectedVariant.value = index;
+        }
+        function toggleStock() {
+            inventory.value = inventory.value > 0 ? 0 : 100;
+        }
 
 
         return {
             product,
+            brand,
             productDescription,
             image,
             productLink,
             inStock,
             inventory,
             onSale,
+            saleMessage,
             details,
             variants,
+            selectedVariant,
             sizes,
             sizesInLine,
             cart,
+            title,
             addToCart,
             updateImage,
+            updateVariant,
             toggleStock
         }
     }
