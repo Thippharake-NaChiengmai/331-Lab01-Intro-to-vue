22b06d3cb4d97a97d22b636139441cb57f1d7f99
Thippharake-NaChiengmai <thippharake_na@cmu.ac.th>
2025-06-27 01:03:45 +0700

    point 6.7

--- main.js
+++ main.js
@@ -8,24 +8,38 @@ const app = createApp({
         const productDescription = ref('A pair of warm and comfortable boots for winter.');
         const image = ref('./assets/images/socks_green.jpg');
         const productLink = ref('https://www.camt.cmu.ac.th');
-         const inStock = ref(true);
+         const inStock = computed(() => inventory.value > 0);
         const inventory = ref(100);
-        const onSale = ref(true);
+        const onSale = computed(() => inventory.value > 0);
         const details = ref([
             '50% cotton',
             '30% wool',
             '20% polyester']);
         const variants = ref([
             {
-                id: 2234, color: 'green'
+                id: 2234, color: 'green', image: './assets/images/socks_green.jpg'
             },
             {
-                id: 2235, color: 'blue'
+                id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg'
             }
         ]);
 
+
         const sizes = ref(['S', 'M', 'L']);
         const sizesInLine = computed(() => sizes.value.join('/'));
+         const cart = ref(0);
+        function addToCart() {
+            cart.value += 1;
+        }
+         function updateImage(variantImage) {
+            image.value = variantImage;
+        }
+
+         function toggleStock () {
+      inventory.value = inventory.value > 0 ? 0 : 100;
+    }
+
+
         return {
             product,
             productDescription,
@@ -37,7 +51,11 @@ const app = createApp({
             details,
             variants,
             sizes,
-            sizesInLine
+            sizesInLine,
+            cart,
+            addToCart,
+            updateImage,
+            toggleStock
         }
     }
 
