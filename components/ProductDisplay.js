d7536e0f2d808073ed922ed6d27bb3feb9deed2c
Thippharake-NaChiengmai <thippharake_na@cmu.ac.th>
2025-06-27 10:25:18 +0700

    point 9.9

---
+++ components/ProductDisplay.js
@@ -0,0 +1,114 @@
+const ProductDisplay = {
+  template: /*html*/
+  `
+  <div class="product-display">
+            <div class="product-container">
+            <p>Shipping: {{ Shipping }}</p>
+                <div class="product-image">
+                    <img :src="image" :class="{ 'out-of-stock-image': !inStock }">
+                </div>
+            </div>
+        </div>
+        <div class="product-info">
+            <h1><a :href="productLink" :style="{'text-decoration': 'none', 'color': '#282828'}">{{ title }}</a></h1>
+            <h3>{{ productDescription }}</h3>
+            <p v-if="inventory > 10">In Stock</p>
+            <p v-else-if="inventory <= 10 && inventory > 0">Almost out of Stock</p>
+            <p v-else>Out of Stock</p>
+            <p v-if="onSale" :style="{'color': 'red', 'font-weight': 'bold'}">{{ saleMessage }}</p>
+            <ul>
+                <li v-for="detail in details">{{ detail }}</li>
+                <li>Size: {{ sizesInLine }}</li>
+            </ul>
+             <div v-for="(variant,index) in variants" :key="variant.id" @mouseover="updateVariant(index)"
+                class="color-circle" :style="{ backgroundColor: variant.color }">
+            </div>
+                 <button class="button" :disabled='!inStock' @click="addToCart" :class="{ disabledButton: !inStock }">Add to Cart</button>
+            <button class="button toggle" @click="toggleStock">{{ inventory > 0 ? 'In Stock' : 'Out of Stock' }}</button>
+
+            </div>
+        </div>
+  `,
+     setup(props) {
+        const { ref, computed } = Vue;
+        const product = ref('Boots');
+        const productDescription = ref('A pair of warm and comfortable boots for winter.');
+        const brand = ref('SE 331');
+        const image = computed(() => {
+            return variants.value[selectedVariant.value].image;
+        });
+        const productLink = ref('https://www.camt.cmu.ac.th');
+        const inStock = computed(() => {
+            return variants.value[selectedVariant.value].quantity;
+        });
+        const inventory = ref(100);
+        const onSale = computed(() => {
+            return variants.value[selectedVariant.value].quantity;
+        });
+        const saleMessage = computed(() =>
+  onSale.value ? `${brand.value} ${product.value} is on sale!` : ''
+)
+        const details = ref([
+            '50% cotton',
+            '30% wool',
+            '20% polyester']);
+        const variants = ref([
+            {
+                id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50
+            },
+            {
+                id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0
+            }
+        ]);
+        const selectedVariant = ref(0);
+        const sizes = ref(['S', 'M', 'L']);
+        const sizesInLine = computed(() => sizes.value.join('/'));
+        const title = computed(() => {
+            return brand.value + ' ' + product.value;
+        });
+        function addToCart() {
+            cart.value += 1;
+        }
+        function updateImage(variantImage) {
+            image.value = variantImage;
+        }
+
+        function updateVariant(index) {
+            selectedVariant.value = index;
+        }
+        function toggleStock() {
+            inventory.value = inventory.value > 0 ? 0 : 100;
+        }
+
+        const Shipping = computed(() => {
+          if (props.premium) {
+            return 'Free';
+          } 
+          return '30';
+        });
+
+        return {
+            product,
+            brand,
+            productDescription,
+            Shipping,
+            image,
+            productLink,
+            inStock,
+            inventory,
+            onSale,
+            saleMessage,
+            details,
+            variants,
+            selectedVariant,
+            sizes,
+            sizesInLine,
+            title,
+            addToCart,
+            updateImage,
+            updateVariant,
+            toggleStock
+        }
+    }
+
+}
--- main.js
+++ main.js
@@ -1,82 +1,17 @@
 // const product = 'Socks'
 const { createApp, ref, computed } = Vue;
-
 const app = createApp({
     setup() {
-        // const product = ref('Socks');
-        const product = ref('Boots');
-        const productDescription = ref('A pair of warm and comfortable boots for winter.');
-        const brand = ref('SE 331');
-        const image = computed(() => {
-            return variants.value[selectedVariant.value].image;
-        });
-        const productLink = ref('https://www.camt.cmu.ac.th');
-        const inStock = computed(() => {
-            return variants.value[selectedVariant.value].quantity;
-        });
-        const inventory = ref(100);
-        const onSale = computed(() => {
-            return variants.value[selectedVariant.value].quantity;
-        });
-        const saleMessage = computed(() =>
-  onSale.value ? `${brand.value} ${product.value} is on sale!` : ''
-)
-        const details = ref([
-            '50% cotton',
-            '30% wool',
-            '20% polyester']);
-        const variants = ref([
-            {
-                id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50
-            },
-            {
-                id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0
-            }
-        ]);
-        const selectedVariant = ref(0);
-        const sizes = ref(['S', 'M', 'L']);
-        const sizesInLine = computed(() => sizes.value.join('/'));
         const cart = ref(0);
-        const title = computed(() => {
-            return brand.value + ' ' + product.value;
-        });
-        function addToCart() {
-            cart.value += 1;
-        }
-        function updateImage(variantImage) {
-            image.value = variantImage;
-        }
-
-        function updateVariant(index) {
-            selectedVariant.value = index;
-        }
-        function toggleStock() {
-            inventory.value = inventory.value > 0 ? 0 : 100;
-        }
-
-
+        const premium = ref(true);
         return {
-            product,
-            brand,
-            productDescription,
-            image,
-            productLink,
-            inStock,
-            inventory,
-            onSale,
-            saleMessage,
-            details,
-            variants,
-            selectedVariant,
-            sizes,
-            sizesInLine,
             cart,
-            title,
-            addToCart,
-            updateImage,
-            updateVariant,
-            toggleStock
+            premium
         }
     }
+})
+.component('product-display', ProductDisplay)
+.mount('#app')
 
-}).mount('#app')
+
+        
